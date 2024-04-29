fc81d7517c189d0ff53ec821a321ed08d32c392c
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-04-29 18:05:30 +0530

    designed header and some homepage ui components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,10 +8,15 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white font-semibold text-3xl">Welcome Back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap=3 mt-4">
-            <ListItem/>
+            <ListItem image="/images/like.png" name="Liked Songs" href="liked"/>
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+      </div>
     </div>
   );
 }
