e3fd2ce5a139066f44092869266ff15cb52d9ff7
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-04-30 01:55:06 +0530

    added track upload feature and fixed some bugs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,29 +1,38 @@
-"use client"
+"use client";
 
 import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
-
-    const authModal=useAuthModal()
-    const{user}=useUser()
-    const onClick=()=>{
-
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
     }
-    return ( <div className="flex flex-col">
-        <div className="flex items-center justify-between px-5 pt-4">
-            <div className="inline-flex items-center gap-x-2">
-                <TbPlaylist className="text-neutral-400" size={26}/>
-                <p className="text-neutral-400 font-medium text-md">My Library</p>
-            </div>
-            <AiOutlinePlus onClick={onClick} className="text-neutral-400 cursor-pointer hover:text-white transition" size={20}/>
+
+    return uploadModal.onOpen();
+  };
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p className="text-neutral-400 font-medium text-md">My Library</p>
         </div>
-        <div className="flex flex-col gap-y-4 mt-4 px-4">
-            List of tracks!
-        </div>
-    </div> );
-}
- 
+        <AiOutlinePlus
+          onClick={onClick}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          size={20}
+        />
+      </div>
+      <div className="flex flex-col gap-y-4 mt-4 px-4">List of tracks!</div>
+    </div>
+  );
+};
+
 export default Library;
